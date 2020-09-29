8c253390e6fb1bacae059b5a3d0b6664ed5461cc
Alex Pavlov <foliant230@gmail.com>
2020-09-29 22:10:57 +0300

    add: complete all test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || !array.length) {
+    return 0;
+  }
+
+  array = array.sort((a, b) => {
+    return a - b;
+  });
+  
+  return array[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || !array.length) {
+    return 0;
+  }
+
+  array = array.sort((a, b) => {
+    return b - a;
+  });
+  
+  return array[0];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || !array.length) {
+    return 0;
+  }
+
+  let sumOfArgs = array.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+  });
+
+  let average = sumOfArgs / array.length;
+  return average;
+
 }
